6ce557eb6959346672125a9fff0c57ec12e66d38
Baris Kuran <baris.kuran@finixio.com>
2023-10-17 17:33:12 +0100

    clean 30

--- _RAW/tests/typeOf.js
+++ _RAW/tests/typeOf.js
@@ -1,13 +1,13 @@
 import typeOf from "../lib/_typeOf";
 
-test("Returns type of variable", () => {
-    expect(typeOf("str")).toBe("string");
-    expect(typeOf(true)).toBe("boolean");
-    expect(typeOf([1, 2, 3])).toBe("array");
-    expect(typeOf({ test1: "test" })).toBe("object");
-    expect(typeOf()).toBe("undefined");
-    expect(typeOf(null)).toBe("null");
-    expect(typeOf(5)).toBe("number");
-    expect(typeOf(546568235688652)).toBe("number");
-    expect(typeOf(() => {})).toBe("function");
-});
+// test("Returns type of variable", () => {
+//     expect(typeOf("str")).toBe("string");
+//     expect(typeOf(true)).toBe("boolean");
+//     expect(typeOf([1, 2, 3])).toBe("array");
+//     expect(typeOf({ test1: "test" })).toBe("object");
+//     expect(typeOf()).toBe("undefined");
+//     expect(typeOf(null)).toBe("null");
+//     expect(typeOf(5)).toBe("number");
+//     expect(typeOf(546568235688652)).toBe("number");
+//     expect(typeOf(() => {})).toBe("function");
+// });
