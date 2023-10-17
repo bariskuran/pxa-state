97a7fb62aebee3e54702dd1e55f0b398ff06b2a0
Baris Kuran <baris.kuran@finixio.com>
2023-10-17 20:30:22 +0100

    update

--- src/usePxaContext.js
+++ src/usePxaContext.js
@@ -1,8 +1,11 @@
-const usePxaContext = (fn, context) => {
+import createPxaContext from "./createPxaContext";
+
+const usePxaContext = (fn = (s) => [s.set], context = createPxaContext()) => {
     const FN_STR = fn.toString();
     const STATE_KEY = FN_STR.charAt(0) === "(" ? FN_STR.match(/\((\w+)\)=>/)[1] : FN_STR.match(/(\w+)=>/)[1];
     const regex = new RegExp(`${STATE_KEY}\\.(\\w+)`, "g");
-            const keys = [...FN_STR.matchAll(regex)].map((match) => match[1]);
+    const keys = [...FN_STR.matchAll(regex)].map((match) => match[1]);
     return keys.map((key) => context((s = {}) => s[key]));
 };
+
 export default usePxaContext;
